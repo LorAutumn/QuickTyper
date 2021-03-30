b8b437e0c7ca243fc6ebdebbb0ad2494371948eb
61251850 <LorAutumn@users.noreply.github.com>
2021-03-30 21:32:20 +0200

    added api fetch for random words an implemented words data array to word processor

--- src/App.js
+++ src/App.js
@@ -1,29 +1,52 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+// TODO : insert counter
 
 export default function App() {
     const [t, setT] = useState('')
     const [word, setWord] = useState('hi')
     const [count, setCount] = useState(0)
     const wordLength = t.length - 1
+    const wordApi = 'https://random-word-api.herokuapp.com/word?number=1000'
+    const [words, setWords] = useState([]) // fetched words from api
 
+    //fetches 1000 random words from api on app initial load
+    useEffect(() => {
+        axios
+            .get(wordApi)
+            .then(res => {
+                setWords(res.data)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }, [])
+
+    // returns random int between min and max
+    const getRandomInt = (min, max) => {
+        return Math.floor(Math.random() * (max - min + 1) + min)
+    }
+
+    //returns alert whhit count of correct typed words an sets a new word, resets setT and resets the counter
     const gameOver = () => {
         alert(`Game Over! You got ${count} words`)
-        setWord('hi')
+        setWord(words[getRandomInt(0, 1000)])
         setT('')
         setCount(0)
     }
 
+    //loads new word afters all letter where typed correctly or return gameOver() after a wrong letter was typed
     const wordProcesser = () => {
         if (t === '') return
-        if (t[wordLength] !== word[wordLength]) {
-            gameOver()
-        }
+        if (t[wordLength] !== word[wordLength]) return gameOver()
         if (t === word) {
-            setWord('ho')
+            setWord(words[getRandomInt(0, 1000)])
             setT('')
         }
     }
 
+    // counts up if a complete word was typed correctly
     const counterLogic = () => {
         if (t === word) setCount(count + 1)
     }
@@ -42,7 +65,7 @@ export default function App() {
                     className='input'
                     onChange={e => setT(e.target.value)}></input>
                 <p className='word-count'>word count: {count}</p>
-                {t === word ? <div>correct!</div> : null}
+                {/*t === word ? <div>correct!</div> : null*/}
             </div>
         </div>
     )
