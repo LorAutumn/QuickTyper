8451bd561bddf491da99c9475a6c4662f5e6f99c
61251850 <LorAutumn@users.noreply.github.com>
2021-03-31 23:03:10 +0200

    added countdown timer of 10 seconds on every word

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import Timer from 'tiny-timer'
 
 // TODO : insert counter
 
@@ -10,6 +11,7 @@ export default function App() {
     const wordLength = t.length - 1
     const wordApi = 'https://random-word-api.herokuapp.com/word?number=1000'
     const [words, setWords] = useState([]) // fetched words from api
+    const [counter, setCounter] = useState(10)
 
     //fetches 1000 random words from api on app initial load
     useEffect(() => {
@@ -23,17 +25,30 @@ export default function App() {
             })
     }, [])
 
-    // returns random int between min and max
-    const getRandomInt = (min, max) => {
-        return Math.floor(Math.random() * (max - min + 1) + min)
-    }
+    // 10 seconds timer
+    useEffect(() => {
+        if (counter > 0) {
+            const timer =
+                counter > 0 && setInterval(() => setCounter(counter - 1), 1000)
+            return () => clearInterval(timer)
+        } else gameOver()
+    }, [counter])
 
-    //returns alert whhit count of correct typed words an sets a new word, resets setT and resets the counter
+    useEffect(() => {
+        setCounter(10)
+    }, [word])
+
+    //returns alert whith count of correct typed words and sets a new word, resets setT and resets the counter
     const gameOver = () => {
         alert(`Game Over! You got ${count} words`)
         setWord(words[getRandomInt(0, 1000)])
         setT('')
         setCount(0)
+    }
+
+    // returns random int between min and max
+    const getRandomInt = (min, max) => {
+        return Math.floor(Math.random() * (max - min + 1) + min)
     }
 
     //loads new word afters all letter where typed correctly or return gameOver() after a wrong letter was typed
@@ -59,6 +74,7 @@ export default function App() {
             <div className='wrapper'>
                 <h1>Word Guess !!</h1>
                 <h2>Type as fast as you can!</h2>
+                <p>Time left: {counter} seconds</p>
                 <h3>{word}</h3>
                 <input
                     value={t}
